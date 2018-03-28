fadf270d790786749d5f4234c6c66d8d3f8b9899
Christian Dyl <christian.dyl@outlook.com>
2018-03-28 15:20:17 -0400

    setup_server

---
+++ setup_server.sh
@@ -0,0 +1,28 @@
+!/bin/bash
+
+IMAGE_NAME=$1
+PLATFORM=$2
+SERVICE_ID=$3
+SECRET_KEY=$4
+
+ADMINIX_IMAGES_PATH=/tmp/adminix_images
+
+if [ -d "$ADMINIX_IMAGES_PATH" ]; then
+  rm -rf $ADMINIX_IMAGES_PATH
+fi
+
+mkdir $ADMINIX_IMAGES_PATH
+curl -L https://github.com/adminixio/images/tarball/master | tar -xz --strip-components=1 -C $ADMINIX_IMAGES_PATH
+
+if [ -d "$ADMINIX_IMAGES_PATH/$IMAGE_NAME/$PLATFORM" ]; then
+  echo "Setting up a server..."
+  sh $ADMINIX_IMAGES_PATH/$IMAGE_NAME/${PLATFORM}/bin/setup.sh
+else
+  echo "Wrong image name"
+fi
+
+if [ ! -z "$SERVICE_ID" ] && [ ! -z "$SECRET_KEY" ]; then
+  echo "Generating Adminix credentials file..."
+  mkdir -p ~/.config/adminix
+  echo "{\"service_id\":\"${SERVICE_ID}\",\"secret_key\":\"${SECRET_KEY}\"}" >> ~/.config/adminix/credentials
+fi
